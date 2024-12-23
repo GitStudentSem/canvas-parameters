ad81c7d98d70a5a0e13a365ede5e05565517fdf5
GitStudentSem <69849493+GitStudentSem@users.noreply.github.com>
2024-12-23 19:03:54 +0300

    try to fix types

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,38 @@
-import type { InputsInfo, Options, Position } from "canvas-parameters";
+export type InputsInfo = Array<
+	Omit<Partial<HTMLInputElement>, "value" | "name"> & {
+		/** uniq name for the control */
+		name: string;
+		/** The value for the input */
+		value: string | number;
+		// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+		onChange: (value: any) => void;
+	}
+>;
+
+export interface Position {
+	top?: CSSStyleDeclaration["top"];
+	left?: CSSStyleDeclaration["left"];
+}
+
+export interface Options {
+	/**
+	 * Is show parameters by default
+	 * @default true
+	 */
+	defaultVisible?: boolean;
+
+	/**
+	 * HTML event for show parameters
+	 * @default "dblclick"
+	 */
+	onShowEvent?: string;
+
+	/** For clear canvas or new init function */
+	onUpdateCanvas?: () => void;
+
+	/** Positon of the parameters on page */
+	position?: Position;
+}
 
 export default class CanvasParameters {
 	parametersWrapper?: HTMLDivElement;
